abd803b4f5aaaa39342b5dac6f367d8ca2155e4e
Gabriel Loutfalla Kanawati <gabrielkanawati3@gmail.com>
2023-01-27 18:30:47 -0300

    frontend - Create Header and Table of days

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,21 @@
-import { Habit } from './components/Habit'
 import './styles/global.css'
+import { Plus } from 'phosphor-react'
+import Header from './components/Header'
+import SummaryTable from './components/SummaryTable'
 
-function App() {
+// import { Habit } from './components/Habit'
+
+
+export function App() {
   return (
-    <>
-      <h1>Ola mundo</h1>
-      <Habit completed={1} />
-    </>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+
+        <SummaryTable />
+
+      </div>
+    </div>
   )
 }
 
-export default App
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Plus } from 'phosphor-react'
+
+import logoImage from '../assets/logo.svg'
+
+
+export default function Header() {
+  return (
+    <div>
+      <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+        <img src={logoImage} alt="habits" />
+
+        <button
+          type='button'
+          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+        >
+          <Plus size={20} className='text-violet-500' />
+          Novo hábito
+        </button>
+      </div>
+    </div>
+  )
+}
